579b979c93680e2978bce01b76b123cbb3001c0c
James E.H. Turner <jturner@gemini.edu>
2015-05-29 22:35:34 -0300

    Add some DataFileList tests.

--- quadpype/tests/test_data.py
+++ quadpype/tests/test_data.py
@@ -1,7 +1,12 @@
+import pytest
 import os.path
 import numpy as np
 from astropy.utils.data import get_pkg_data_filename
 from ..data import FileName, DataFile, DataFileList
+
+
+# Data in common to some of the tests below:
+fn_mefnodq = get_pkg_data_filename('data/eqbprgS20120827S0069_flat.fits')
 
 
 def test_FileName_gemini_IRAF_1():
@@ -41,10 +46,9 @@ def test_DataFile_gemini_IRAF_new_1():
            df.filename.standard == True and \
            len(df) == 0
 
-def test_DataFile_gemini_IRAF_existing():
+def test_DataFile_gemini_IRAF_existing_1():
 
-    fn = get_pkg_data_filename('data/eqbprgS20120827S0069_flat.fits')
-    df = DataFile(filename=fn)
+    df = DataFile(filename=fn_mefnodq)
 
     # Ignore the directory name because the above call translates it to the
     # absolute location of this package on the system.
@@ -53,8 +57,52 @@ def test_DataFile_gemini_IRAF_existing():
            df.filename.suffix == ['_flat'] and \
            df.filename.ext == 'fits' and \
            df.filename.standard == True and \
+           df.meta['OBJECT'] == 'GCALflat' and \
            len(df) == 2 and \
-           abs(np.mean(df[0]) - 1.0623) < 0.0002 and \
-           abs(np.mean(df[1]) - 0.9461) < 0.0002
+           abs(np.mean(df[0]) - 1.0623) < 0.0001 and \
+           abs(np.mean(df[1]) - 0.9461) < 0.0001
 
+def test_DataFileList_gemini_IRAF_existing_1():
+
+    dfl = DataFileList(filenames=fn_mefnodq)
+
+    assert len(dfl) == 1 and \
+           dfl[0].filename.prefix == 'eqbprg' and \
+           dfl[0].filename.base == 'S20120827S0069' and \
+           dfl[0].filename.suffix == ['_flat'] and \
+           dfl[0].filename.ext == 'fits' and \
+           dfl[0].filename.standard == True and \
+           dfl[0].meta['OBJECT'] == 'GCALflat' and \
+           len(dfl[0]) == 2 and \
+           abs(np.mean(dfl[0][0]) - 1.0623) < 0.0001 and \
+           abs(np.mean(dfl[0][1]) - 0.9461) < 0.0001
+
+def test_DataFileList_replacing_data_1():
+
+    # Replace the data & header from file with blank ones:
+    dfl = DataFileList(data=[], meta={}, filenames=fn_mefnodq)
+
+    assert len(dfl) == 1 and len(dfl[0]) == 0 and len(dfl[0].meta) == 0
+
+def test_DataFileList_len_mismatch_1():
+
+    # Cannot have multiple data objects per filename:
+    with pytest.raises(ValueError):
+        dfl = DataFileList(data=[DataFile(), DataFile()], filenames=fn_mefnodq)
+
+def test_DataFileList_broadcast_data_1():
+
+    # But we can map the same dataset to multiple files:
+    dfl = DataFileList(data=DataFile(), filenames=[fn_mefnodq, 'test_name'])
+
+    # Produces 2 separate DataFiles referring to the same data array.
+    assert len(dfl) == 2 and dfl[0] is not dfl[1] \
+        and dfl[0].data is dfl[1].data
+
+def test_DataFileList_copy_self_1():
+
+    dfl1 = DataFileList(filenames=fn_mefnodq)
+    dfl2 = DataFileList(dfl1)
+
+    assert dfl1 is not dfl2 and dfl1 == dfl2
 
