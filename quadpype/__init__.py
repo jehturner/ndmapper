6a5b128fc442bab47237906248b21b77cbcbecef
James E.H. Turner <jturner@gemini.edu>
2015-07-24 06:55:53 -0300

    Apply Bryan's plotting hang workaround posted on the forum on 17 June 2015.

--- quadpype/__init__.py
+++ quadpype/__init__.py
@@ -19,7 +19,13 @@ config = {'filename_regex' : '[NS][0-9]{8}S[0-9]{3,4}',
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
+
     # from example_mod import *
     import re
     config['filename_regex'] = re.compile(config['filename_regex'])
+
+    # Set PyRAF options in the environment before it gets imported.
+    # This is to solve the plotting hang on Apple reported by Bryan M.
+    import os
+    os.environ['PYRAF_BETA_STATUS'] = '1'
 
