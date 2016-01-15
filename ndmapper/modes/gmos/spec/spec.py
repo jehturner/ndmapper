eda2c200b049d8d39bf2019256349ddc77840d87
James E.H. Turner <jturner@gemini.edu>
2016-01-15 20:48:23 -0300

    Add missing dependence on 'arc' for flats & standards.

--- ndmapper/modes/gmos/spec/spec.py
+++ ndmapper/modes/gmos/spec/spec.py
@@ -6,8 +6,8 @@ from ..gmos import *
 # Default calibration dependence for GMOS spectroscopy. The 'spectwilight'
 # type is left out for now, as it's currently not used here.
 CAL_DEPS = {'target' : ['specphot', 'flat', 'arc', 'bias'],
-            'specphot' : ['flat', 'bias'],
-            'flat' : ['bias'],
+            'specphot' : ['flat', 'arc', 'bias'],
+            'flat' : ['arc', 'bias'],
             'arc' : ['bias'],
             'bias' : []
            }
