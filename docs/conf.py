7d8f199931f164b2edbcff10c1d5baa3c6b4472c
James E.H. Turner <jturner@gemini.edu>
2016-02-12 21:11:36 -0300

    Try using mock module to avoid pyraf import in readthedocs, as per their FAQ.

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,16 @@ import datetime
 import os
 import sys
 
+# JT: From http://read-the-docs.readthedocs.org/en/latest/faq.html:
+from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['pyraf']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 try:
     import astropy_helpers
 except ImportError:
