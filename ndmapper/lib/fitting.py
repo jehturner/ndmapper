3b1534c44dd7eda27bdfe63e9fa62646245c37cd
James E.H. Turner <jturner@gemini.edu>
2016-06-24 22:16:31 -0400

    Implement iterative pixel rejection (replacing rejected pixels with the fit rather than excluding them, which astropy.modelling doesn't allow for). Initial test fit results are at most <1% different from IRAF fit1d.

--- ndmapper/lib/fitting.py
+++ ndmapper/lib/fitting.py
@@ -1,6 +1,7 @@
 # Copyright(c) 2016 Association of Universities for Research in Astronomy, Inc.
 # by James E.H. Turner.
 
+import math
 import numpy as np
 from astropy.modeling import models, fitting
 
@@ -10,12 +11,12 @@ function_map = {
     'polynomial' : models.Polynomial1D,
 }
 
-def fit_1D(arr, function='legendre', order=1, axis=-1, lsigma=3.0, hsigma=3.0,
+def fit_1D(image, function='legendre', order=1, axis=-1, lsigma=3.0, hsigma=3.0,
            iterations=0):
     """
     An routine for evaluating the result of fitting 1D polynomials to each
-    vector along some axis of an N-dimensional image array, with (not yet)
-    iterative pixel rejection and re-fitting, similar to IRAF's fit1d.
+    vector along some axis of an N-dimensional image array, with iterative
+    pixel rejection and re-fitting, similar to IRAF's fit1d.
 
     Only a subset of fit1d functionality is currently supported (not
     including interactivity).
@@ -23,7 +24,7 @@ def fit_1D(arr, function='legendre', order=1, axis=-1, lsigma=3.0, hsigma=3.0,
     Parameters
     ----------
 
-    arr : `ndarray`
+    image : `ndarray`
         N-dimensional input array containing the values to be fitted.
 
     function : {'legendre'}, optional
@@ -56,40 +57,69 @@ def fit_1D(arr, function='legendre', order=1, axis=-1, lsigma=3.0, hsigma=3.0,
 
     # Determine how many pixels we're fitting each vector over:
     try:
-        npix = arr.shape[axis]
+        npix = image.shape[axis]
     except IndexError:
         raise ValueError('axis={0} out of range for input shape {1}'\
-                         .format(axis, arr.shape))
+                         .format(axis, image.shape))
 
     # To support fitting any axis of an N-dimensional array, first convert the
     # input array to a 2D stack of 1D rows (a no-op for 2D images with axis=-1)
     # -- make a list of dimension numbers, move the specified dim to the end,
     # transpose the array accordingly, and then flatten all dimensions prior to
     # the last one into a single axis:
-    ndim = len(arr.shape)
+    ndim = len(image.shape)
     dims = range(ndim)
     dims.append(dims.pop(axis))
-    arr = arr.transpose(dims)
-    newshape = arr.shape
-    arr = arr.reshape(-1, npix)
+    image = image.transpose(dims)
+    newshape = image.shape
+    image = image.reshape(-1, npix)
 
-    # Perform the fits "simultaneously" with AstroPy's modelling module:
-    nfits = arr.shape[0]
+    # If applicable, make a temp copy of the image for cleaned pixels:
+    clean = image if iterations == 0 else image.copy()
+
+    # Prepare to perform the fits "simultaneously" with AstroPy modelling:
+    nfits = image.shape[0]
     points = np.arange(npix)
+    points_2D = np.tile(points, (nfits,1))
     models = function_map[function](degree=order-1, n_models=nfits)
     fitter = fitting.LinearLSQFitter()
-    models = fitter(models, points, arr)
 
-    # Evaluate the fits at each pixel:
-    points_2D = np.tile(points, (nfits,1))
-    arr = models(points_2D)
+    # Create a mask for tracking rejected pixel values:
+    mask_2D = np.zeros_like(image, dtype=np.bool)
+
+    # (Re-)fit the data with rejection of outlying points:
+    for n in range(iterations+1):
+
+        # Fit the pixel data:
+        models = fitter(models, points, clean)
+
+        # Evaluate the fits at each pixel:
+        fitvals = models(points_2D)
+
+        # Replace deviant pixels in each row with the fitted values:
+        for n, (row, fit, mask) in enumerate(zip(clean, fitvals, mask_2D)):
+            diff = row - fit
+            stddev = math.sqrt(np.mean(diff*diff))
+            rejpix = np.where((diff > hsigma*stddev) | (diff < -lsigma*stddev))
+            mask[rejpix] = True
+            row[mask] = fit[mask]  # replace cumulative rejections with new fit
+            lastdev = stddev
+
+    # # TEST: Plot the fit:
+    # import pylab
+    # nrow = 3087
+    # mask = mask_2D[nrow]
+    # pylab.plot(points, image[nrow], 'k.')
+    # pylab.plot(points, fitvals[nrow])
+    # pylab.plot(points[mask], image[nrow][mask], 'rx')
+    # pylab.show()
 
     # Restore the original ordering & shape of the (substitute) array:
-    arr = arr.reshape(newshape)
+    fitvals = fitvals.reshape(newshape)
     dims = range(ndim)
     dims.insert(axis, dims[-1])  # insert before pop to handle [-1] properly
     del dims[-1]
-    arr = arr.transpose(dims)
+    fitvals = fitvals.transpose(dims)
 
-    return arr
+    return fitvals
 
