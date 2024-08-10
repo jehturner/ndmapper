77df0e274efa1e7273f7910658d3cb6406dad1f5
James E.H. Turner <jturner@gemini.edu>
2024-08-09 21:18:58 -0400

    Remove more vestiges of astropy_helpers.

--- ndmapper/__init__.py
+++ ndmapper/__init__.py
@@ -1,15 +1,9 @@
-# Copyright(c) 2015 Association of Universities for Research in Astronomy, Inc.
+# Copyright(c) 2015-2024 Association of Universities for Research in Astronomy, Inc.
 # by James E.H. Turner.
 
 """
 A simple package to automate pushing data through a series of processing steps.
 """
-
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
 
 config = {'filename_regex' : '[NS][0-9]{8}S[0-9]{3,4}',
           'logfile' : None,
@@ -20,41 +14,38 @@ config = {'filename_regex' : '[NS][0-9]{8}S[0-9]{3,4}',
           'interact' : False
          }
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
+import re
+config['filename_regex'] = re.compile(config['filename_regex'])
 
-    # from example_mod import *
-    import re
-    config['filename_regex'] = re.compile(config['filename_regex'])
+# Set PyRAF options in the environment before it gets imported.
+# This is to solve the plotting hang on Apple reported by Bryan M.
+import os
+os.environ['PYRAF_BETA_STATUS'] = '1'
 
-    # Set PyRAF options in the environment before it gets imported.
-    # This is to solve the plotting hang on Apple reported by Bryan M.
-    import os
-    os.environ['PYRAF_BETA_STATUS'] = '1'
+# Define a decorator that converts some package-API-standard arguments to
+# use package run-time default values, unless specified explicitly:
+from functools import wraps
+import inspect
 
-    # Define a decorator that converts some package-API-standard arguments to
-    # use package run-time default values, unless specified explicitly:
-    from functools import wraps
-    import inspect
-    def ndprocess_defaults(proc_fn):
+def ndprocess_defaults(proc_fn):
 
-        @wraps(proc_fn)  # transfer target function name & docstring to wrapper
+    @wraps(proc_fn)  # transfer target function name & docstring to wrapper
 
-        # Wrapper function to be substituted for the original:
-        def wrap_defaults(*args, **kwargs):
+    # Wrapper function to be substituted for the original:
+    def wrap_defaults(*args, **kwargs):
 
-            # Evaluate what arguments the processing function we're wrapping
-            # would receive if we called it directly:
-            callargs = inspect.getcallargs(proc_fn, *args, **kwargs)
+        # Evaluate what arguments the processing function we're wrapping
+        # would receive if we called it directly:
+        callargs = inspect.getcallargs(proc_fn, *args, **kwargs)
 
-            # Convert any unspecified flags to the run-time defaults specified
-            # in the package configuration dictionary:
-            for param in ['interact', 'reprocess', 'use_uncert', 'use_flags']:
-                if param in callargs and callargs[param] is None:
-                    callargs[param] = config[param]
+        # Convert any unspecified flags to the run-time defaults specified
+        # in the package configuration dictionary:
+        for param in ['interact', 'reprocess', 'use_uncert', 'use_flags']:
+            if param in callargs and callargs[param] is None:
+                callargs[param] = config[param]
 
-            # Call the processing function on our modified arguments:
-            return proc_fn(**callargs)
+        # Call the processing function on our modified arguments:
+        return proc_fn(**callargs)
 
-        return wrap_defaults
+    return wrap_defaults
 
